e6ca29f55f86e97f43c10fc62c61f9cf106596dd
garimachhikara128 <garimachhikara128@gmail.com>
2025-08-08 16:42:50 +0530

    Recursion

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -2,6 +2,38 @@
 
 using namespace std ;
 
+void subsequences(string ques, string ans)
+{
+    if(ques.length() == 0)
+    {
+        cout << ans << endl ;
+        return ;
+    }
+
+    char ch = ques[0] ; // a
+    string roq = ques.substr(1) ; // bc
+
+    subsequences(roq, ans) ;
+    subsequences(roq, ans+ch) ;
+}
+
+void permutation(string ques, string ans)
+{
+    if(ques.length() == 0)
+    {
+        cout << ans << endl ;
+        return ;
+    }
+
+    for(int i = 0 ; i < ques.length() ; i++)
+    {
+        char ch = ques[i] ;
+        string roq = ques.substr(0,i) + ques.substr(i+1) ;
+
+        permutation(roq, ans + ch) ;
+    }
+}
+
 int climbingStairs(int curr, string ans, int n)
 {
     if(curr == n)
@@ -42,22 +74,24 @@ void lexicoCounting(int curr, int n)
 
 }
 
-void subsequences(string ques, string ans)
+bool isPalindrome(string str)
 {
-    if(ques.length() == 0)
+    int i = 0 ; 
+    int j = str.length()-1 ;
+
+    while(i < j)
     {
-        cout << ans << endl ;
-        return ;
+        if(str[i] != str[j])
+            return false ;
+
+        i++ ;
+        j-- ;
     }
 
-    char ch = ques[0] ; // a
-    string roq = ques.substr(1) ; // bc
-
-    subsequences(roq, ans) ;
-    subsequences(roq, ans+ch) ;
+    return true ;
 }
 
-void permutation(string ques, string ans)
+void palindromicPartition(string ques, string ans)
 {
     if(ques.length() == 0)
     {
@@ -65,13 +99,61 @@ void permutation(string ques, string ans)
         return ;
     }
 
-    for(int i = 0 ; i < ques.length() ; i++)
+    for(int i = 1 ; i <= ques.length() ; i++)
     {
-        char ch = ques[i] ;
-        string roq = ques.substr(0,i) + ques.substr(i+1) ;
-
-        permutation(roq, ans + ch) ;
+        string part = ques.substr(0,i) ;
+        string roq = ques.substr(i) ;
+        if(isPalindrome(part))
+            palindromicPartition(roq, ans + part + " ") ;
     }
+}
+
+vector<int> diffWaysToCompute(string str) {
+        
+    vector<int> mr ;
+
+    if(str.find('+') == string::npos && str.find('-') == string::npos && str.find('*') == string::npos)
+    {
+        mr.push_back(stoi(str)) ;
+        return mr ;
+    }
+
+    for(int i = 0 ; i < str.length() ; i++)
+    {
+        char ch = str[i] ;
+
+        if(ch == '+' || ch == '-' || ch == '*')
+        {
+            vector<int> rr1 = diffWaysToCompute(str.substr(0,i)) ;
+            vector<int> rr2 = diffWaysToCompute(str.substr(i+1)) ;
+
+            for(int val1 : rr1)
+            {
+                for(int val2: rr2)
+                {
+                    if(ch == '+')
+                        mr.push_back(val1+val2) ;
+                    else if(ch == '-')
+                        mr.push_back(val1-val2) ;
+                    else if(ch == '*')
+                        mr.push_back(val1*val2) ;
+                }
+            }
+        }
+
+    }
+
+    return mr ;
+}
+
+void TOH(int n, string src, string dst, string helper)
+{
+    if(n == 0)
+        return ;
+
+    TOH(n-1, src, helper, dst) ;
+    cout << "move " << n << " disc from " << src << " to " << dst << endl ;
+    TOH(n-1, helper, dst, src) ;
 }
 
 int main()
@@ -80,7 +162,14 @@ int main()
     // permutation("aab", "") ;
     // cout << climbingStairs(0, "", 20) << endl  ;
     // cout << ways << endl ;
-    lexicoCounting(0,1000) ;
+    // lexicoCounting(0,1000) ;
+    // palindromicPartition("abccb", "") ;
+    vector<int> res = diffWaysToCompute("2*3-4*5") ;
+
+    for(int val: res)
+        cout << val << endl ;
+
+    // TOH(3, "S", "D", "H") ;
     return 0 ;
 }
 
